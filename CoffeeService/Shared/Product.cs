f7d188a21ac54f97397323851636659c2afe072c
Evge-s <evgenii.semaniuk@gmail.com>
2022-03-31 23:31:18 +0300

    Add project files.

---
+++ CoffeeService/Shared/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeService.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
