0d412ff7427ae30ad20e3c78567c7144afb940f2
Evge-s <evgenii.semaniuk@gmail.com>
2022-05-05 11:51:01 +0300

    Register Validation

--- CoffeeService/Shared/User/UserRegister.cs
+++ CoffeeService/Shared/User/UserRegister.cs
@@ -1,9 +1,16 @@
-﻿namespace CoffeeService.Shared.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeService.Shared.User
 {
     public class UserRegister
     {
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+
+        [Required, StringLength(100, MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
+
+        [Compare("Password", ErrorMessage = "The password do not match")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
