1940903793e39d40d9e3956f1a606b10344b9c4f
Evge-s <evgenii.semaniuk@gmail.com>
2022-05-10 21:44:11 +0300

    registred AuthenticationStateProvider DI, added CascadingAuthenticationState componnecnt

--- CoffeeService/Client/Program.cs
+++ CoffeeService/Client/Program.cs
@@ -5,6 +5,7 @@ global using CoffeeService.Client.Services.CategoryService;
 global using CoffeeService.Client.Services.CartService;
 global using CoffeeService.Client.Services.AuthService;
 global using System.Net.Http.Json;
+global using Microsoft.AspNetCore.Components.Authorization;
 using CoffeeService.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -20,5 +21,8 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
