33893299da44a74be2bf5dc03ad0db920d0d4485
Evge-s <evgenii.semaniuk@gmail.com>
2022-05-10 23:11:33 +0300

    added ChangePassword endpoint, client service

--- CoffeeService/Server/Controllers/AuthController.cs
+++ CoffeeService/Server/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CoffeeService.Server.Controllers
 {
@@ -34,6 +36,20 @@ namespace CoffeeService.Server.Controllers
         public async Task<ActionResult<ServiceResponse<string>>> Login(UserLogin request)
         {
             var response = await _authService.Login(request.Email, request.Password);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("change-password"), Authorize]
+        public async Task<ActionResult<ServiceResponse<bool>>> ChangePassword([FromBody] string newPassword)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var response = await _authService.ChangePassword(int.Parse(userId), newPassword);
+
             if (!response.Success)
             {
                 return BadRequest(response);
