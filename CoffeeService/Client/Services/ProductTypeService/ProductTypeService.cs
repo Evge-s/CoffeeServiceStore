50c290cd4860ff23e5770b30ba24ea50091e8bd2
Evge-s <evgenii.semaniuk@gmail.com>
2022-07-03 13:30:15 +0300

    Add & Update Product Types on the Client side

--- CoffeeService/Client/Services/ProductTypeService/ProductTypeService.cs
+++ CoffeeService/Client/Services/ProductTypeService/ProductTypeService.cs
@@ -13,11 +13,36 @@
 
         public event Action OnChange;
 
+        public async Task AddProductType(ProductType productType)
+        {
+            var response = await _http.PostAsJsonAsync("api/producttype", productType);
+            ProductTypes = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<ProductType>>>()).Data;
+            OnChange.Invoke();
+        }
+
+        public ProductType CreteNewProductType()
+        {
+            var newProductType = new ProductType { IsNew = true, IsEditing = true };
+
+            ProductTypes.Add(newProductType);
+            OnChange.Invoke();
+            return newProductType;
+        }
+
         public async Task GetProductTypes()
         {
             var result = await _http
                 .GetFromJsonAsync<ServiceResponse<List<ProductType>>>("api/producttype");
             ProductTypes = result.Data;
+        }
+
+        public async Task UpdateProductType(ProductType productType)
+        {
+            var response = await _http.PutAsJsonAsync("api/producttype", productType);
+            ProductTypes = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<ProductType>>>()).Data;
+            OnChange.Invoke();
         }
     }
 }
