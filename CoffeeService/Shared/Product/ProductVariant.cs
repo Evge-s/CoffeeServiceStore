4f6da5923bfade08ce53b20e3f01e2d67a2b2b2c
Evge-s <evgenii.semaniuk@gmail.com>
2022-07-20 19:01:23 +0300

    fix buttons and ProductVariant model

--- CoffeeService/Shared/Product/ProductVariant.cs
+++ CoffeeService/Shared/Product/ProductVariant.cs
@@ -6,9 +6,9 @@ namespace CoffeeService.Shared
     public class ProductVariant
     {
         [JsonIgnore]
-        public Product Product { get; set; }
+        public Product? Product { get; set; }
         public int ProductId { get; set; }
-        public ProductType ProductType { get; set; }
+        public ProductType? ProductType { get; set; }
         public int ProductTypeId { get; set; }
 
         [Column(TypeName="decimal(18,2)")]
